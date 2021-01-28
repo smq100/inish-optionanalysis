3909506852745b45b262547c05bcb50f16a2519b
Steve Quinlan <smq100@gmail.com>
2021-01-28 10:50:23 -0600

    Profit array progress

--- pricing/european.py
+++ pricing/european.py
@@ -26,7 +26,7 @@ class EuropeanPricing(PricingBase):
         self.initialize_variables()
         self.log_parameters()
 
-    def calculate_prices(self, time_to_maturity=0):
+    def calculate_prices(self, time_to_maturity=0.0):
         ''' Calculate Call and Put option prices based on the below equations from Black-Scholes.
         If dividend is not zero, then it is subtracted from the risk free rate in the below calculations.
 
@@ -34,11 +34,12 @@ class EuropeanPricing(PricingBase):
             PutOptionPrice  = Strike*exp(−r(T−t)) *N(−d2) − SpotPrice*N(−d1)
         :return: <float>, <float> Calculated price of Call & Put options
         '''
+
         if time_to_maturity <= 0.0:
             time_to_maturity = self.time_to_maturity
 
-        d_1 = self._calculate_d1()
-        d_2 = self._calculate_d2()
+        d_1 = self._calculate_d1(time_to_maturity)
+        d_2 = self._calculate_d2(time_to_maturity)
 
         self.cost_call = ((self.spot_price * np.exp(-1 * self.dividend * time_to_maturity)) *
                 stats.norm.cdf(d_1, 0.0, 1.0) -
@@ -50,18 +51,19 @@ class EuropeanPricing(PricingBase):
                (self.spot_price * np.exp(-1 * self.dividend * time_to_maturity)) *
                stats.norm.cdf(-1 * d_1, 0.0, 1.0))
 
-        logging.info('Calculated value for European Call Option is $%.2f ', self.cost_call)
-        logging.info('Calculated value for European Put Option is $%.2f ', self.cost_put)
+        # logging.info('Calculated value for European Call Option is $%.2f ', self.cost_call)
+        # logging.info('Calculated value for European Put Option is $%.2f ', self.cost_put)
 
         return self.cost_call, self.cost_put
 
     def generate_profit_table(self, call_put, price):
         ''' TODO '''
 
-        # Ensure prices have been calculated prior
-        valid = False
         table = []
+        valid = False
         type_call = True
+
+        # Ensure prices have been calculated prior
         if call_put.upper() == 'CALL':
             if self.cost_call > 0.0:
                 valid = True
@@ -83,47 +85,58 @@ class EuropeanPricing(PricingBase):
                     if today.isoweekday() <= 5:
                         table.append({str(today): 0.0})
 
+                # Remove last item (days_to_maturity == 0)
+                table.pop()
+
+                # Calculate cost of option every day till expiry
                 for item in table:
                     key, value = list(item.items())[0]
                     maturity_date = datetime.datetime.strptime(key, '%Y-%m-%d %H:%M:%S')
                     time_to_maturity = (self.expiry - maturity_date)
-                    days_to_maturity = time_to_maturity.days / 365
-                    self.calculate_prices(days_to_maturity)
+                    decimaldays_to_maturity = time_to_maturity.days / 365
+                    self.calculate_prices(decimaldays_to_maturity)
 
                     if type_call:
-                        item[key] = self.cost_call
+                        value = self.cost_call
+                        item[key] = value
                     else:
-                        item[key] = self.cost_put
+                        value = self.cost_put
+                        item[key] = value
 
-                    # print(item)
-                    print(key, days_to_maturity, self.cost_call, self.cost_put)
+                    print(f'{key}, {decimaldays_to_maturity:.5f}, ${value:.2f}')
 
         return table
 
 
-    def _calculate_d1(self):
+    def _calculate_d1(self, time_to_maturity=0.0):
         ''' Famous d1 variable from Black-Scholes model calculated as shown in:
 
                 https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model
         :return: <float>
         '''
+        if time_to_maturity <= 0.0:
+            time_to_maturity = self.time_to_maturity
+
         d_1 = (np.log(self.spot_price / self.strike_price) +
-            (self.risk_free_rate - self.dividend + 0.5 * self.volatility ** 2) * self.time_to_maturity) / \
-            (self.volatility * np.sqrt(self.time_to_maturity))
+            (self.risk_free_rate - self.dividend + 0.5 * self.volatility ** 2) * time_to_maturity) / \
+            (self.volatility * np.sqrt(time_to_maturity))
 
         logging.debug('Calculated value for d1 = %f', d_1)
 
         return d_1
 
-    def _calculate_d2(self):
+    def _calculate_d2(self, time_to_maturity=0.0):
         ''' Famous d2 variable from Black-Scholes model calculated as shown in:
 
                 https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model
         :return: <float>
         '''
+        if time_to_maturity <= 0.0:
+            time_to_maturity = self.time_to_maturity
+
         d_2 = (np.log(self.spot_price / self.strike_price) +
-            (self.risk_free_rate - self.dividend - 0.5 * self.volatility ** 2) * self.time_to_maturity) / \
-            (self.volatility * np.sqrt(self.time_to_maturity))
+            (self.risk_free_rate - self.dividend - 0.5 * self.volatility ** 2) * time_to_maturity) / \
+            (self.volatility * np.sqrt(time_to_maturity))
 
         logging.debug('Calculated value for d2 = %f', d_2)
 
