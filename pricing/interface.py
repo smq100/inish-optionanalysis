4ceed77a1ce66e6b5166a7b0a44bfcfc48ceed3e
Steve Quinlan <smq100@gmail.com>
2021-01-29 10:51:38 -0600

    Interface

--- pricing/interface.py
+++ pricing/interface.py
@@ -7,19 +7,24 @@ from european import EuropeanPricing
 class Interface():
     '''TODO'''
     def __init__(self):
-        pd.options.display.float_format = '{:,.2f}'.format
+        self.symbol = []
+        self.legs = []
+        self.strategy = 'long_call'
+        self.method = 'black-scholes'
         self.pricer = None
         self.table_value = pd.DataFrame()
         self.table_profit = pd.DataFrame()
 
-    def calculate(self, ticker, expiry_date, strike, method='black-scholes', dividend=0.0):
-        '''TODO'''
-        if method == 'black-scholes':
-            self.pricer = EuropeanPricing(ticker, expiry_date, strike)
+        pd.options.display.float_format = '{:,.2f}'.format
 
-        call_price, put_price = self.pricer.calculate_prices()
-        self.table_value = self.pricer.generate_value_table('call')
-        self.table_profit = self.pricer.generate_profit_table(call_price, self.table_value)
+    def calculate(self):
+        '''TODO'''
+        if self._validate():
+            if self.method == 'black-scholes':
+                self.pricer = EuropeanPricing(self.symbol['ticker'], self.legs[0]['expiry'], self.legs[0]['strike'])
+                call_price, put_price = self.pricer.calculate_prices()
+                self.table_value = self.pricer.generate_value_table(self.legs[0]['call_put'])
+                self.table_profit = self.pricer.generate_profit_table(call_price, self.table_value)
 
     def main_menu(self):
         '''Displays opening menu'''
@@ -54,7 +59,7 @@ class Interface():
             elif selection == '4':
                 pass
             elif selection == '5':
-                ui.calculate('AAPL', datetime.datetime(2021, 2, 12), 145)
+                self.calculate()
             elif selection == '6':
                 self.plot_value()
             elif selection == '7':
@@ -63,6 +68,18 @@ class Interface():
                 break
             else:
                 print('Unknown operation selected')
+
+    def set_symbol(self, ticker, volatility=-1, dividend=0.0):
+        '''TODO'''
+        self.symbol = {'ticker':ticker, 'volitility':volatility, 'dividend':dividend}
+
+    def clear_legs(self):
+        '''TODO'''
+        self.legs = []
+
+    def add_leg(self, quantity, call_put, long_short, strike, expiry_date):
+        '''TODO'''
+        self.legs.append({'quantity':quantity, 'call_put':call_put, 'long_short':long_short, 'strike': strike, 'expiry': expiry_date})
 
     def plot_value(self):
         '''TODO'''
@@ -74,6 +91,14 @@ class Interface():
         print('\n***** Profit Table *****')
         print(self.table_profit)
 
+    def _validate(self):
+        '''TODO'''
+        return True
+
 if __name__ == '__main__':
     ui = Interface()
+    ui.clear_legs()
+    ui.set_symbol('AAPL')
+    ui.add_leg(1, 'call', 'long', 145.0, datetime.datetime(2021, 2, 12))
+
     ui.main_menu()
