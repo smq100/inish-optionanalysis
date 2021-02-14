d01078c10cc0f9d8535fbc17a8aa5852cba67b12
Steve Quinlan <smq100@gmail.com>
2021-02-13 20:07:22 -0600

    technical analysis

--- analysis/technical.py
+++ analysis/technical.py
@@ -5,7 +5,6 @@ import datetime
 import yfinance as yf
 import pandas as pd
 from ta import trend
-from ta.utils import dropna
 
 from pricing.fetcher import validate_ticker
 
@@ -16,7 +15,7 @@ class TechnicalAnalysis():
     def __init__(self, ticker, start=None):
         if (validate_ticker(ticker)):
             self.ticker = ticker.upper()
-            self.macd_ = 0.0
+            self.macd_ = None
 
             if start is None:
                 self.history = yf.Ticker(ticker).history(period="max")
@@ -24,19 +23,20 @@ class TechnicalAnalysis():
                 self.history = yf.Ticker(ticker).history(start=f'{start:%Y-%m-%d}')
 
     def macd(self):
+        col = ['Diff', 'MACD', 'Signal']
         macd_diff = trend.macd_diff(self.history['Close'], fillna=True)
         macd_ = trend.macd(self.history['Close'], fillna=True)
         macd_signal = trend.macd_signal(self.history['Close'], fillna=True)
-        df = pd.concat([macd_diff, macd_, macd_signal], axis=1)
 
-        col = ['Diff', 'MACD', 'Signal']
+        df = pd.concat([macd_diff, macd_, macd_signal], axis=1)
         df.columns = col
         self.macd_ = df
+
         return df
 
 
 if __name__ == '__main__':
-    start = datetime.datetime.today() + datetime.timedelta(days=-365)
+    start = datetime.datetime.today() - datetime.timedelta(days=365)
     ta = TechnicalAnalysis('BBY', start)
     data = ta.macd()
 
