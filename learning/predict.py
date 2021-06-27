b634c05ba2a73f177515473c240219ad5ef268ed
Steve Quinlan <smq100@gmail.com>
2021-06-27 14:21:02 -0500

    Learning WIP

--- learning/predict.py
+++ learning/predict.py
@@ -1,20 +1,17 @@
-import datetime as dt
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
-from tensorflow.python.keras.backend import shape
+# from tensorflow.python.keras.backend import shape
 
 from data import store as store
 from utils import utils as utils
 
-ticker = 'AAPL'
+_logger = utils.get_logger()
 
-start = dt.datetime(2016, 1, 1)
-end = dt.datetime.now()
+ticker = 'FB'
 
 data = store.get_history(ticker, 5000)
 
@@ -24,10 +21,8 @@ scaled_data = scaler.fit_transform(data['close'].values.reshape(-1,1))
 
 prediction_days = 60
 
-x_train, y_train = [], []
-for x in range(prediction_days, len(scaled_data)):
-    x_train.append(scaled_data[x-prediction_days:x, 0])
-    y_train.append(scaled_data[x, 0])
+x_train = [scaled_data[x-prediction_days:x, 0] for x in range(prediction_days, len(scaled_data))]
+y_train = [scaled_data[x, 0] for x in range(prediction_days, len(scaled_data))]
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
@@ -42,8 +37,12 @@ model.add(LSTM(units=50))
 model.add(Dropout(0.2))
 model.add(Dense(units=1))
 
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=25, batch_size=32)
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10, batch_size=32)
+
+val_loss, val_acc = model.evaluate(x_train, y_train)
+_logger.info(f'{val_loss = }')
+_logger.info(f'{val_acc = }')
 
 # Test
 test_data = store.get_history(ticker, 1000)
@@ -55,10 +54,7 @@ model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_da
 model_inputs = model_inputs.reshape(-1, 1)
 model_inputs = scaler.fit_transform(model_inputs)
 
-x_test = []
-for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x-prediction_days:x, 0])
-
+x_test = [model_inputs[x-prediction_days:x, 0] for x in range(prediction_days, len(model_inputs))]
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
