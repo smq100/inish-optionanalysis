c9a80dbab6d785e44403735764d5f887bd884b89
Steve Quinlan <smq100@gmail.com>
2021-06-27 09:12:54 -0500

    Learning WIP

--- learning/predict.py
+++ learning/predict.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
+from tensorflow.python.keras.backend import shape
 
 from data import store as store
 from utils import utils as utils
@@ -15,7 +16,7 @@ ticker = 'AAPL'
 start = dt.datetime(2016, 1, 1)
 end = dt.datetime.now()
 
-data = store.get_history(ticker, 1000)
+data = store.get_history(ticker, 5000)
 
 # Prepare data
 scaler = MinMaxScaler(feature_range=(0,1))
@@ -24,16 +25,14 @@ scaled_data = scaler.fit_transform(data['close'].values.reshape(-1,1))
 prediction_days = 60
 
 x_train, y_train = [], []
-
 for x in range(prediction_days, len(scaled_data)):
-    x_train.append(scaled_data[x-prediction_days])
-    y_train.append(scaled_data[x,0])
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # Create neural net
-
 model = Sequential()
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(Dropout(0.2))
@@ -46,9 +45,33 @@ model.add(Dense(units=1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
+# Test
+test_data = store.get_history(ticker, 1000)
+actual_prices = test_data['close'].values
+
+total_dataset = pd.concat((data['close'], test_data['close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.fit_transform(model_inputs)
+
+x_test = []
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+prediction_prices = model.predict(x_test)
+prediction_prices = scaler.inverse_transform(prediction_prices)
+
 
 
 if __name__ == '__main__':
-    from logging import DEBUG
-    print(data.head())
-
+    plt.plot(actual_prices, color='black', label='Actual')
+    plt.plot(prediction_prices, color='green', label='Predict')
+    plt.title(f'{ticker} Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.legend(loc='upper left')
+    plt.show()
