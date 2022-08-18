7f0fa0c7cb72f5a3d15e1d46a0cd08b789572bc4
Steve Quinlan <smq100@gmail.com>
2022-08-18 08:41:57 -0500

    Cache module updates

--- utils/cache.py
+++ utils/cache.py
@@ -41,12 +41,13 @@ def load(name: str, type: str, today_only: str = True) -> tuple[object, str]:
     if not type:
         raise AssertionError('Must include \'type\'')
 
+    name = name.lower()
+    type = type.lower()
+
     object = None
     filename = ''
     date = dt.datetime.now().strftime(ui.DATE_FORMAT)
 
-    name = name.lower()
-    type = type.lower()
     names = get_filenames(name, type)
     if not names:
         pass
@@ -101,32 +102,42 @@ def roll(type: str) -> tuple[bool, str]:
     message = ''
     rolled = 0
 
-    paths = get_filenames('', type)
-    for result_old in paths:
-        file_old = f'{CACHE_BASEPATH}/{result_old}.{CACHE_SUFFIX}'
-        date_time = dt.datetime.now().strftime(ui.DATE_FORMAT)
-        result_new = f'{date_time}{result_old[10:]}'
-        if result_new > result_old:
-            file_new = f'{CACHE_BASEPATH}/{result_new}.{CACHE_SUFFIX}'
-            try:
-                os.replace(file_old, file_new)
-            except OSError as e:
-                success = False
-                message = f'File error for {e.filename}: {e.strerror}'
-                break
-            else:
-                rolled += 1
+    files = get_filenames('', type)
+    if files:
+        for result_old in files:
+            date = dt.datetime.now().strftime(ui.DATE_FORMAT)
+            head, _, _ = result_old.partition('.')
+            parts = head.split('_')
+            result_new = f'{date}_{parts[1]}_{parts[2]}'
 
-    if success:
-        message = f'Rolled {rolled} file(s)'
-        _logger.info(f'{__name__}: {message}')
+            if result_new > result_old:
+                file_old = f'{CACHE_BASEPATH}/{result_old}.{CACHE_SUFFIX}'
+                file_new = f'{CACHE_BASEPATH}/{result_new}.{CACHE_SUFFIX}'
+                try:
+                    os.replace(file_old, file_new)
+                except OSError as e:
+                    success = False
+                    message = f'File error for {e.filename}: {e.strerror}'
+                    break
+                else:
+                    rolled += 1
+
+        if success:
+            message = f'Rolled {rolled} file(s)'
+            _logger.info(f'{__name__}: {message}')
+        else:
+            _logger.error(f'{__name__}: {message}')
     else:
-        _logger.error(f'{__name__}: {message}')
+        success = False
+        message = 'No files'
 
     return success, message
 
 
 def delete(type: str) -> tuple[bool, str]:
+    if not type:
+        raise AssertionError('Must include \'type\'')
+
     type = type.lower()
     success = True
     message = ''
@@ -171,10 +182,10 @@ def delete(type: str) -> tuple[bool, str]:
 
 
 def get_filenames(name: str, type: str, type_only: bool = False) -> list[str]:
-    files = []
     name = name.lower()
     type = type.lower()
 
+    files = []
     for item in os.scandir(CACHE_BASEPATH):
         if item.is_file():
             head, sep, tail = item.name.partition('.')
@@ -184,9 +195,9 @@ def get_filenames(name: str, type: str, type_only: bool = False) -> list[str]:
                 pass # Bad filename structure
             elif tail != CACHE_SUFFIX:
                 pass # Wrong file type
-            elif name and name != parts[1]:
+            elif name and name != parts[2]:
                 pass # Bad name
-            elif type == parts[2]:
+            elif type == parts[1]:
                 files += [head]
 
     # Return most recent first
@@ -200,8 +211,10 @@ def build_filename(name: str, type: str) -> str:
     if not type:
         raise ValueError('\'type\' must be valid')
 
+    name = name.lower()
+    type = type.lower()
     date_time = dt.datetime.now().strftime(ui.DATE_FORMAT)
-    filename = f'{CACHE_BASEPATH}/{date_time}_{name.lower()}_{type.lower()}.{CACHE_SUFFIX}'
+    filename = f'{CACHE_BASEPATH}/{date_time}_{type}_{name}.{CACHE_SUFFIX}'
 
     return filename
 
